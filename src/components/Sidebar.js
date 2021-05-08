a6a9805733b9ccae9e64b7236cfdd2f558880281
Rokas <rookas.rudzenskas@gmail.com>
2021-05-08 16:20:23 +0300

    Rokas

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import "./styles/Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
+                <Avatar />
 
+                <div className="sidebar__headerRight">
+                    <IconButton><DonutLargeIcon /></IconButton>
+                    <IconButton><ChatIcon /></IconButton>
+                    <IconButton><MoreVertIcon /></IconButton>
+                </div>
             </div>
 
             <div className="sidebar__search">
