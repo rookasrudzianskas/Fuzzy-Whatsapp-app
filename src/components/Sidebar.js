b2c929ed89546d98486d629b2c9a3803c11331a8
Rokas <rookas.rudzenskas@gmail.com>
2021-05-09 17:08:16 +0300

    Rokas

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./styles/Sidebar.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
@@ -7,8 +7,30 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 // import SearchIcon from '@material-ui/icons/Search';
 import {SearchOutlined} from "@material-ui/icons";
 import SidebarChat from "./SidebarChat";
+import db from "../firebase";
 
 const Sidebar = () => {
+
+    const [rooms, setRooms] = useState();
+
+    useEffect(() => {
+    //     we run just once to attach the firebase listener
+    //    we go to the collection rooms, and take the picture of the snapshot, take the all data of rooms
+    //    and also on all changes take the snapshot and update the data, it is all the data which is inside
+        db.collection("rooms").onSnapshot(snapshot => (
+            // so this sets the ROom state, to the docs object, with the id and the name as the array of the objects
+            // so it goes per all the id's and takes the objects, in this case, it has just the id and the name
+            // and adds the objects to the state of rooms
+            setRooms(snapshot.docs.map(doc =>
+                ({
+                    // this reffers to the id in the room id, in the middle one
+                    id: doc.id,
+                    // this is in the id all the data, in tis case just the name
+                    data: doc.data(),
+                })
+            ))
+        ));
+    }, []);
     return (
         <div className="sidebar">
             <div className="sidebar__header">
