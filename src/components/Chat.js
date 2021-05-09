737f6e22912151497c3d95504dfe4a1401ff2d3b
Rokas <rookas.rudzenskas@gmail.com>
2021-05-09 16:13:10 +0300

    Rokas

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -20,6 +20,10 @@ const Chat = () => {
         <div className="chat">
             <div className="chat__header">
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at ....................</p>
+                </div>
             </div>
 
             <div className="chat__body">
