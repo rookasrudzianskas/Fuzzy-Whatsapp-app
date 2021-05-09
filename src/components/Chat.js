71424e1685a61d721a093f7c0521413ce55eed82
Rokas <rookas.rudzenskas@gmail.com>
2021-05-09 16:12:18 +0300

    Rokas

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./styles/Chat.css";
+import {Avatar} from "@material-ui/core";
 
 const Chat = () => {
+    // we store the avatar here
+    const [seed, setSeed] = useState('');
+
+    useEffect(() => {
+
+        // just generate the random number and store in the state
+        // we are making the random number for every user, and then we pass it as the seed to generate the aavatar
+        // and by using this numbers as the seeds we generate the random avatar for the system
+        // the number between 0 and 5000
+        setSeed(Math.floor(Math.random() * 5000))
+    //    after that we set the seed to that number, and it gets the array of the random generated numbers
+    }, []);
+
     return (
         <div className="chat">
-            
+            <div className="chat__header">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+            </div>
+
+            <div className="chat__body">
+
+            </div>
+
+            <div className="chat__footer">
+
+            </div>
         </div>
     );
 };
