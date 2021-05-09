99635ef044b1bc9f9a14276f9967e629294d043b
Rokas <rookas.rudzenskas@gmail.com>
2021-05-09 16:33:16 +0300

    Rokas

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -36,6 +36,12 @@ const Chat = () => {
             </div>
 
             <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Rookas Rudzianskas</span>
+                    Hey guys
+                    <span className="chat__timestamp">3.45PM</span>
+                </p>
+
 
             </div>
 
