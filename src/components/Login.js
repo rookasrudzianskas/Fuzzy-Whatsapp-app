3be4642b683eff330aa2521f163f28ed7bf242be
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 06:32:11 +0300

    Roaks

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,13 +2,23 @@ import React from 'react';
 import "./styles/Login.css";
 import {Button} from "@material-ui/core";
 import {auth, provider} from "../firebase";
+import {useStateValue} from "../StateProvider";
+import {actionTypes} from "../reducer";
 
 const Login = () => {
+    // dispatch shoots the data to the data layer
+    const [{}, dispatch] = useStateValue();
 
     const signIn = () => {
     //     sign in function
         auth.signInWithPopup(provider).then(result => {
-            console.log(result);
+            // we send this data to the data layer,
+            // the action type, we add the action type object, and then the payload as the user, with the result object, it received from Google
+            // and it becomes the user object inside the data layer
+            dispatch({
+                type: actionTypes.SET_USER,
+                user: result.user,
+            });
         }).catch(error => alert(error.message));
     };
 
